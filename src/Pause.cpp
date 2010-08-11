63857c860cc7a2e22e56d260608f46fcfefd4cfb
Diego D. Galizzi <dgalizzi@gmail.com>
2010-08-11 19:45:54 -0300

    Fixed mute button not showing when game paused.

--- src/Pause.cpp
+++ src/Pause.cpp
@@ -1,10 +1,10 @@
-/* 
+/*
 ** Copyright (c) 2010, Diego D. Galizzi
 **
 ** Permission to use, copy, modify, and/or distribute this software for any
 ** purpose with or without fee is hereby granted, provided that the above
 ** copyright notice and this permission notice appear in all copies.
-** 
+**
 ** THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 ** WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 ** MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -37,19 +37,19 @@ void Pause::ProcessEvent(sf::Event &theEvent)
 		{
 			End();
 		}
-	}	
+	}
+	mBoard->SwitchMute(theEvent);
 }
 
 void Pause::Step()
 {
 	mBoard->DrawBackground();
+	mBoard->DoMusic(16, 16);
 	mBoard->DrawScore();
-	
+
 	// Draw pause
 	mRender->Draw(mPause);
-	
+
 	mRender->Display();
 	mRender->Clear(sf::Color(240, 240, 240));
 }
-
-
